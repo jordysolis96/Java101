307545adf2c71570003ee6a51b1ac21094a84c9d
jordy muniz <jordy.muniz96@gmail.com>
2021-02-11 11:54:36 -0600

    sum exercise

---
+++ src/edabitChallenges.java
@@ -0,0 +1,12 @@
+public class edabitChallenges {
+    public static void main(String[] args) {
+
+    }
+
+//    Create a method that takes two integers as arguments and return their sum.
+    public static class SumOfTwoNumbers {
+        public static int sum(int a, int b) {
+            return a + b;
+        }
+    }
+}
