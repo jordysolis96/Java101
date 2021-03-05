24010e74af03ee13a4d22f04b1e01ea14fe30373
jordy muniz <jordy.muniz96@gmail.com>
2021-03-05 12:48:19 -0600

    triangle exercise complete

--- src/edabitChallenges.java
+++ src/edabitChallenges.java
@@ -131,4 +131,10 @@ public static class Circuit {
     }
 }
 
+//    Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.
+public static class Triangle {
+    public static int nextEdge(int side1, int side2) {
+        return (side1 + side2) - 1;
+    }
+}
 }
