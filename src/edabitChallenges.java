b8945804e50ce121a20d86b648fd64e7588e102d
jordy muniz <jordy.muniz96@gmail.com>
2021-03-27 20:06:27 -0500

    increment challange

--- src/edabitChallenges.java
+++ src/edabitChallenges.java
@@ -137,4 +137,9 @@ public static class Triangle {
         return (side1 + side2) - 1;
     }
 }
+
+//    Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+        public static int addition(int num) {
+            return num + 1;
+    }
 }
