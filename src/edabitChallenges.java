235096b300f15e3798063fa1fe7d9faa86dde35e
jordy muniz <jordy.muniz96@gmail.com>
2021-04-07 11:43:32 -0500

    football game challenge

--- src/edabitChallenges.java
+++ src/edabitChallenges.java
@@ -205,4 +205,12 @@ public class edabitChallenges {
     public static int animals(int chickens, int cows, int pigs) {
         return chickens * 2 + (cows + pigs) * 4;
     }
+//    Create a function that takes the number of wins, draws and losses and calculates the number of points a football team has obtained so far.
+//
+//    wins get 3 points
+//    draws get 1 point
+//    losses get 0 points
+    public static int footballPoints(int wins, int draws, int loses) {
+        return wins*3 + draws;
+    }
 }
