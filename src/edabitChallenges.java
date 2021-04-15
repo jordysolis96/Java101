c1dffc20b3208dd95c865c05f30244ec05d45ecf
jordy muniz <jordy.muniz96@gmail.com>
2021-04-14 22:30:55 -0500

    equal numbers

--- src/edabitChallenges.java
+++ src/edabitChallenges.java
@@ -226,4 +226,10 @@ public class edabitChallenges {
             return false;
         }
     }
+//    Create a function that takes two integers and checks if they are equal.
+    public static boolean isEqual(int num1, int num2) {
+        if(num1 == num2){
+            return true;
+        }
+    }
 }
