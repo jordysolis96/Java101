8bec271c23bd88a3827df1a289471287e391ad4a
jordy muniz <jordy.muniz96@gmail.com>
2021-04-15 16:46:35 -0500

    challenge fixed

--- src/edabitChallenges.java
+++ src/edabitChallenges.java
@@ -228,8 +228,10 @@ public class edabitChallenges {
     }
 //    Create a function that takes two integers and checks if they are equal.
     public static boolean isEqual(int num1, int num2) {
-        if(num1 == num2){
+         if(num1 == num2){
             return true;
-        }
+        }else{
+             return false;
+         }
     }
 }
